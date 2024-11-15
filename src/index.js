2f6c4292dac4abdac82806fa1f75bdd20ba7c1b9
hit7sh <hit7sh@gmail.com>
2024-11-15 13:05:13 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -14,29 +14,33 @@ app.use(express.json());
 app.post('/run', async (req, res) => {
     const { code, inputText = '', language } = req.body;
     console.log({ code });
-    let base_url = '', docker_image = '';
+    let base_url = '', docker_image = '', code_extension = '';
     if (language === 'c_cpp') {
         base_url = '~/dsa-ladder-backend/cpp/code';
         docker_image = 'gcc';
+        code_extension = 'cpp';
     }
     else if (language === 'python') {
         base_url = '~/dsa-ladder-backend/python/code'
         docker_image = 'python';
+        code_extension = 'py';
     }
     else if (language === 'javascript') {
         base_url = '~/dsa-ladder-backend/nodejs/code'
         docker_image = 'node';
+        code_extension = 'js';
     }
     else if (language === 'java') {
         base_url = '~/dsa-ladder-backend/java/code';
         docker_image = 'openjdk';
+        code_extension = 'java';
     }
 
     try {
         await fs.writeFile(`${base_url}/runtime_errors.txt`, '');
         await fs.writeFile(`${base_url}/compile_errors.txt`, '');
         await fs.writeFile(`${base_url}/output.txt`, '');
-        await fs.writeFile(`${base_url}/index.cpp`, code);
+        await fs.writeFile(`${base_url}/index.${code_extension}`, code);
         await fs.writeFile(`${base_url}/input.txt`, inputText);
     } catch (err) {
         console.log({ 'duringWrite': err });
