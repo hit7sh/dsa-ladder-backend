1df47814da93a40941ddf7ad5c9d2808949045dd
hit7sh <hit7sh@gmail.com>
2024-11-15 17:04:03 +0530

    update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const express = require('express')
 const cors = require('cors');
 
 const mongoose = require('mongoose');
-const { User } = require('./userSchema');
+const User = require('./userSchema');
 
 const app = express();
 
