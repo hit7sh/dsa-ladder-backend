069b5a7d8fec5dd4d788bd91cb2dc1ad2e013ff5
hit7sh <hit7sh@gmail.com>
2024-11-26 02:13:34 +0530

    submit code

--- src/evaluateCode.js
+++ src/evaluateCode.js
@@ -17,7 +17,33 @@ export const runCode = async ({ res, code, inputText, language, }) => {
     return res.json(await codeRunner({ res, code, inputText }));
 };
 
+const compareOutputs = (str1, str2) => {
+    const cleanStr1 = str1.replace(/\s+/g, '');
+    const cleanStr2 = str2.replace(/\s+/g, '');
+    return cleanStr1 === cleanStr2;
+};
+
 export const submitCode = async ({ res, code, language, testCases, }) => {
     const codeRunner = getCodeRunner({ language });
-    return res.json({ testCases });
+    const response = [];
+    await Promise.all(testCases?.map(async ({ input, output: expectedOutput, }) => {
+        const { output, compile_errors, runtime_errors } = await codeRunner({ res, code, inputText: input, })
+
+        if (runtime_errors || compile_errors) {
+            response.push({
+                verdict: 'ER',
+            });
+            return;
+        }
+        if (!compareOutputs(expectedOutput, output)) {
+            response.push({
+                verdict: 'WA'
+            });
+            return;
+        }
+        response.push({
+            verdict: 'AC',
+        });
+    }));
+    res.json({ response });
 };
